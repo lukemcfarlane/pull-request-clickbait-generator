46980eeb93a95aebc9828dc45aeceac02bb8c74e
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2016-05-13 13:56:07 +1200

    Add thor.

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ Pull Request Clickbait Notifier
 ===============================
 
 Generates clickbait messages in order to notify people on Slack about pull requests.
+
+```
+Usage:
+  main.rb notify
+
+Options:
+  -c, [--channel=CHANNEL]    # Specify the Slack channel
+  -u, [--url=URL]            # Specify the pull request URL
+  -p, [--project=PROJECT]    # Specify the project name (used to generate message)
+  -l, [--language=LANGUAGE]  # Specify the language/framework name (used to generate message)
+
+Notify a Slack channel about a pull request with a randomly-generated clickbait message.
+```
--- main.rb
+++ main.rb
@@ -1,4 +1,16 @@
+require "thor"
 require "./bot"
 
-bot = Bot.new('#channel')
-bot.notify("https://github.com", "Test Project", "React")
+class Notifier < Thor
+  desc "notify", "Notify a Slack channel about a pull request with a randomly-generated clickbait message."
+  method_option :channel, :aliases => "-c", :desc => "Specify the Slack channel"
+  method_option :url, :aliases => "-u", :desc => "Specify the pull request URL"
+  method_option :project, :aliases => "-p", :desc => "Specify the project name (used to generate message)"
+  method_option :language, :aliases => "-l", :desc => "Specify the language/framework name (used to generate message)"
+  def notify
+    bot = Bot.new(options[:channel])
+    bot.notify(options[:url], options[:project], options[:language])
+  end
+end
+
+Notifier.start
